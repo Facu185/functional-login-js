34e255f0f3da24c151947e686804e9fe67c2e1a8
Facu185 <facufa12@gmail.com>
2023-04-27 16:50:13 -0300

    finished js

---
+++ index.js
@@ -0,0 +1,29 @@
+let users = ["facufa12", "santi29", "christian", "diseño"];
+let passwords = ["facu185", "apolo1", "mendndez", "web"];
+const userInput = document.getElementById("user");
+const passwordInput = document.getElementById("password");
+const button = document.getElementById("buttonLogin");
+button.addEventListener("click", login);
+const container = document.getElementById("container");
+function login() {
+  let user = userInput.value;
+  let password = passwordInput.value;
+  let userindex = users.findIndex((e) => e === user);
+  let passwordindex = passwords.findIndex((e) => e === password);
+  if (!user || !password){
+    alert("Alguno de los campos estan vacios");
+    throw new Error("Alguno de los campos estan vacios");
+  }
+  if (userindex || passwordindex === -1) {
+    if (!document.getElementById("errorText")) {
+      const textError = document.createElement("p");
+      textError.innerText = "Alguno de los campos esta incorrecto";
+      textError.id = "errorText";
+      textError.classList = "error";
+      container.append(textError);
+      throw new Error("Alguno de los campos esta incorrecto");
+    }
+  } else if (userindex === passwordindex) {
+    alert("Logeado con exito");
+  }
+}
